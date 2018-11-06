c83181a3f3ea16fc435b4f451c3648822486428a
Debbie Christopher <debbie@tapsplace.com>
2018-11-06 16:46:43 -0600

    inital work

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog80@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __intit__(self, name):
+        self.name = name
+
+tasks = []
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+        
+
+    
+    return render_template('blogpost.html', title="Build A Blog", tasks=tasks)
+
+
+
+if __name__ == '__main__':
+    app.run()
